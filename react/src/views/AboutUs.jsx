5717599c7e731ee2db087ddd865e4b54921eacf1
Ilija Radovanovic <ilija009@gmail.com>
2023-04-18 16:07:24 +0200

    start work on Asortiman page

--- react/src/views/AboutUs.jsx
+++ react/src/views/AboutUs.jsx
@@ -1,5 +1,29 @@
+import Hero from "../components/Hero";
+
 export default function AboutUs() {
     return (
-        <h1>About Us</h1>
+        <>
+            <Hero title="O nama">
+                <p>
+                "Osnivanje maloprodajnog objekta i formiranje specijalizovane prodavnice u vidu gvožđare-farbare 
+                bio je logičan potez osnivača koji je dugo godina usko povezan sa proizvodnjom svih žičanih proizvoda, 
+                raznih mreža (sita) i eksera. Sve je počelo samo idejom, a onda i realizacijom 2012. godine. 
+                Trenutno imamo troje zaposlenih, a ako bude mogućnosti taj broj će se u budućnosti povećavati..."
+                </p>
+            </Hero>
+            <section className="light-section">
+                <div className="inner-container">
+                    <p>Prodajni objekat je površine 35m2, a raspolažemo sa istom tolikom površinom magacinskog prostora.</p>
+
+                    <p>
+                        "U ovom trenutku raspolažemo sa preko 3000 artikala, a u ponudi možemo posebno
+                        istaći žičane proizvode, koje odmeravamo po želji ili se
+                        poručuju i izrađuj u po specijalnim zahtevima cenjenih potrošača."
+                    </p>
+                </div>
+            </section>
+            {/* osmisli kako ide asortiman show page */}
+            {/* osmisli about us page */}
+        </>
     )
 }
--- react/src/views/Asortiman.jsx
+++ react/src/views/Asortiman.jsx
@@ -3,12 +3,38 @@ import Hero from "../components/Hero";
 export default function Asortiman() {
     return (
         <>
-            <Hero title="Sirok asortiman proizvoda">
+            <Hero title="Asortiman proizvoda">
                 <p>Zice, orgrade, lakovi, boje, alat ...</p>
             </Hero>
             <section className="light-section">
                 <div className="inner-container">
                     <h2>Ovde idu kartice sa asortimanom</h2>
+                    <div className="products-container">
+                    <div className="card">
+                        <img src="./images/products/kosilica.webp" alt="elektricna kosilica" />
+                        <p>Elekticna kosilica</p>
+                    </div>
+                    <div className="card">
+                        <img src="./images/products/sac-gusani.webp" alt="elektricna kosilica" />
+                        <p>Sac gusani</p>
+                    </div>
+                    <div className="card">
+                        <img src="./images/products/trimer.webp" alt="elektricna kosilica" />
+                        <p>Trimer kosilica</p>
+                    </div>
+                    <div className="card">
+                        <img src="./images/products/kosilica.webp" alt="elektricna kosilica" />
+                        <p>Elekticna kosilica</p>
+                    </div>
+                    <div className="card">
+                        <img src="./images/products/kosilica.webp" alt="elektricna kosilica" />
+                        <p>Elekticna kosilica</p>
+                    </div>
+                    <div className="card">
+                        <img src="./images/products/kosilica.webp" alt="elektricna kosilica" />
+                        <p>Elekticna kosilica</p>
+                    </div>
+                </div>
                 </div>
             </section>
             {/* osmisli kako ide asortiman show page */}
